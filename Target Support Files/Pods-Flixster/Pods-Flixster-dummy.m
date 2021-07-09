d7cd221b411eeb2970c3283e94a3dd208fcff20c
hallilacanlale <57775171+hallilacanlale@users.noreply.github.com>
2021-07-09 13:23:43 -0500

    Delete Target Support Files directory

--- Target Support Files/Pods-Flixster/Pods-Flixster-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Flixster : NSObject
-@end
-@implementation PodsDummy_Pods_Flixster
-@end
--- Target Support Files/Pods-FlixsterTests/Pods-FlixsterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlixsterTests : NSObject
-@end
-@implementation PodsDummy_Pods_FlixsterTests
-@end
